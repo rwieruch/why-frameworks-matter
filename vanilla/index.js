dcdcbef63ef041142e2759ae3e09d3fabe26da33
Robin Wieruch <wrobin@gmx.net>
2018-02-11 11:08:05 +0100

    split up into es6 and vanilla JS

--- vanilla/index.js
+++ vanilla/index.js
@@ -1,49 +1,60 @@
-const BASE_URL = 'https://hn.algolia.com/api/v1/';
+var BASE_URL = 'https://hn.algolia.com/api/v1/';
 
 function getList(query) {
-  const url = `${BASE_URL}search?query=${query}&hitsPerPage=200`;
+  var url = BASE_URL + 'search?query=' + query + '&hitsPerPage=200';
   return fetch(url)
-    .then(response => response.json())
-    .then(result => result.hits);
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      return result.hits;
+    });
 }
 
-const addButtonEvent = () =>
+function addButtonEvent() {
   document.getElementById('searchButton')
     .addEventListener('click', onSearch);
+};
 
-const onSearch = () => {
+function onSearch() {
   removeList();
 
   doSearch(getElementByIdValue('searchInput'))
     .then(appendList);
 };
 
-const getElementByIdValue = id =>
-  document.getElementById(id).value;
+function getElementByIdValue(id) {
+  return document.getElementById(id).value;
+};
 
-const doSearch = query =>
-  getList(query);
+function doSearch(query) {
+  return getList(query);
+};
 
-const removeList = () => {
-  const listNode = document.getElementById('list');
+function removeList() {
+  var listNode = document.getElementById('list');
 
   if (listNode) {
     listNode.parentNode.removeChild(listNode);
   }
 }
 
-const appendList = list => {
-  const listNode = document.createElement('div');
+function appendList(list) {
+  var listNode = document.createElement('div');
   listNode.setAttribute('id', 'list');
   document.getElementById('app').appendChild(listNode);
 
   list.forEach(appendItem(listNode));
 };
 
-const appendItem = listNode => item => {
-  const itemNode = document.createElement('div');
-  itemNode.appendChild(document.createTextNode(item.title));
-  listNode.appendChild(itemNode);
+function appendItem(listNode) {
+  return function (item) {
+    var itemNode = document.createElement('div');
+    itemNode.appendChild(document.createTextNode(item.title));
+    listNode.appendChild(itemNode);
+  };
 };
 
-(() => addButtonEvent())();
+(function () {
+  addButtonEvent();
+})();
