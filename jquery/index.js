005e5e55443da9930d961ff3fd11a05a955011c5
Anton Kuleshov <antonkuleshov8@gmail.com>
2018-12-25 02:37:20 +0200

    jQuery Contribution

---
+++ jquery/index.js
@@ -0,0 +1,21 @@
+var BASE_URL = 'https://hn.algolia.com/api/v1/';
+
+$('#searchButton').on('click', function() {
+  $('#list').empty();
+  var query = $('#searchInput').val();
+
+  $('#app').append('<div id="list"></div>');
+
+  $.getJSON(BASE_URL + 'search?query=' + query + '&hitsPerPage=200', function(data) {
+    var items = [];
+    $.each(data.hits, function(key, val) {
+      items.push("<li id='" + key + "'>" + val.title + "</li>");
+    });
+
+    $("<ul/>", {
+      "class": "my-new-list",
+      html: items.join("")
+    }).appendTo("#list");
+  });
+
+});
