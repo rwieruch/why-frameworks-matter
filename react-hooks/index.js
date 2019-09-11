81b9ebc6fcafd827061625e890bb58e9650b9eb0
Fabian Rademacher <fabianrademacher@gmx.de>
2019-09-11 17:11:45 +0200

    react-hooks: reformatting

--- react-hooks/index.js
+++ react-hooks/index.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://hn.algolia.com/api/v1/';
 
-function doSearch(query) {
+const doSearch = query => {
   const url = `${BASE_URL}search?query=${query}&hitsPerPage=200`;
   return fetch(url)
     .then(response => response.json())
@@ -11,29 +11,27 @@ const App = () => {
   const [input, setInput] = React.useState('');
   const [list, setList] = React.useState([]);
 
-  const onSubmit = (e) => {
+  const onSubmit = e => {
     e.preventDefault();
-
     doSearch(input)
-      .then((hits) => setList(hits));
+      .then(hits => setList(hits));
   }
 
-  const onChange = (e)  => {
+  const onChange = e =>
     setInput(e.target.value);
-  }
 
-    return (
-      <div>
-        <h1>Search Hacker News with React</h1>
-        <form type="submit" onSubmit={onSubmit}>
-          <input type="text" onChange={onChange} value={input} />
-          <button type="text">Search</button>
-        </form>
-        {list.map(item =>
-          <Item key={item.objectID} item={item} />
-        )}
-      </div>
-    );
+  return (
+    <div>
+      <h1>Search Hacker News with React</h1>
+      <form type="submit" onSubmit={onSubmit}>
+        <input type="text" onChange={onChange} value={input} />
+        <button type="text">Search</button>
+      </form>
+      {list.map(item =>
+        <Item key={item.objectID} item={item} />
+      )}
+    </div>
+  );
 }
 
 const Item = ({ item }) =>
