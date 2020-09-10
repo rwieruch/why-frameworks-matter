2b2a095f007a76d6002790e7e81eb135726124f8
casdidier <cheung.didier@gmail.com>
2020-09-10 16:26:00 +0200

    nextjs implementation

---
+++ nextjs/pages/index.js
@@ -0,0 +1,52 @@
+import Item from "../components/Item";
+
+const BASE_URL = "https://hn.algolia.com/api/v1/";
+
+function doSearch(query) {
+  const url = `${BASE_URL}search?query=${query}&hitsPerPage=200`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((result) => result.hits);
+}
+
+class IndexPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+      list: []
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    doSearch(this.state.input).then((hits) => this.setState({ list: hits }));
+  }
+
+  onChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  render() {
+    const { input, list } = this.state;
+    return (
+      <div>
+        <h1>Search Hacker News with React</h1>
+        <form type="submit" onSubmit={this.onSubmit}>
+          <input type="text" onChange={this.onChange} value={input} />
+          <button type="text">Search</button>
+        </form>
+        {list.map((item) => (
+          <Item key={item.objectID} item={item} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
